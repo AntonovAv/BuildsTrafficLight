1edf2a3e2052eb48795ef7fd9cc44104182e8cc0
antantval@gmail.com <654defoltER123>
2016-05-27 19:41:20 +0300

    minor fix

--- SystemConfig.h
+++ SystemConfig.h
@@ -11,7 +11,7 @@
 #include <EEPROM.h>
 
 #define TRAFFIC_LIGHT_PARAMS_EEPROM_ADDR 0 // lenght 1+1+1 = 3 bytes
-#define BUILD_SERVER_PARAMS_EEPROM_ADDR 3 //lenght 16 + 4 = 19 bytes
+#define BUILD_SERVER_PARAMS_EEPROM_ADDR 3 //lenght 16 + 4 = 20 bytes
 #define SOUND_PARAMS_EEPROM_ADDR 22 // lenght 1 byte
 #define DEBUG_MODE_EEPROM_ADDR 23 // lenght 1 byte
 #define WIFI_PARAMS_EEPROM_ADDR 24 // lenght 64 bytes
